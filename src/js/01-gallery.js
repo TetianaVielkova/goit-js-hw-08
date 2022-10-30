f94088f856d7f44511056f845912fe69b973264b
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-10-30 17:25:57 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const addGalleryItem = createGalleryItem(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', addGalleryItem);
+
+
+
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => 
+
+    `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`).join("");
+}
+
+new SimpleLightbox('.gallery__item', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
