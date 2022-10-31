5643810dac4e3e7e8a9c3afd050245660fbd569c
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-10-31 12:36:22 +0300

    Изменения

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,33 @@
 import throttle from 'lodash.throttle';
 
 const FEEDBACK_FORM = "feedback-form-state";
-
 const formRef = document.querySelector('.feedback-form');
-
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
 
 const formData = {};
 
 formRef.addEventListener('input', throttle(onFormData, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
-
 function onFormData(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
 }
 
-
 function onFormSubmit(e) {
     e.preventDefault();
-    
+    console.log('formData: ', formData);
     e.currentTarget.reset();
-
     localStorage.removeItem(FEEDBACK_FORM);
 }
 
 getMessage();
 
 function getMessage() {
-    const savedData = localStorage.getItem(FEEDBACK_FORM);
-    if(savedData) {
-        const feedback = JSON.parse(savedData);
-
+    const feedback = JSON.parse(localStorage.getItem(FEEDBACK_FORM));
+    if(feedback) {
+        inputRef.value = feedback.email;
+        textareaRef.value = feedback.message;
     }
-    console.log(formData);
 }
